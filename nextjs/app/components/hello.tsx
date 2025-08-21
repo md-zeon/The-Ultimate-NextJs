5e4b8db665470ed975d7846d3380b100855c67ff
md-zeon <zeon.cse@gmail.com>
2025-08-21 01:14:12 +0000

    Understood react server and client component

---
+++ nextjs/app/components/hello.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+function Hello() {
+    console.log("I am a Client Component Right?");
+    return (
+        <button>Hello</button>
+    )
+}
+
+export default Hello;
--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,7 +1,11 @@
+import Hello from "./components/hello";
+
 export default function Home() {
+  console.log("What am I? Server");
   return (
     <div>
       <h1>Welcome to Next js</h1>
+      <Hello />
     </div>
   );
 }
