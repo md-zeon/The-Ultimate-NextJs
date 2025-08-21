fd73bea2745288911f904de725f457109ca8e49b
md-zeon <zeon.cse@gmail.com>
2025-08-21 01:28:52 +0000

    Learned basic Routings of next js

---
+++ nextjs/app/dashboard/users/page.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Users = () => {
+  return (
+    <div>
+        <h1>Users</h1>
+        <div>
+            <li><Link href="/dashboard/users/1">User 1</Link></li>
+            <li><Link href="/dashboard/users/2">User 2</Link></li>
+            <li><Link href="/dashboard/users/3">User 3</Link></li>
+            <li><Link href="/dashboard/users/4">User 4</Link></li>
+        </div>
+    </div>
+  )
+}
+
+export default Users
